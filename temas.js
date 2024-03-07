f42df8356feca7644ff6ebc31726cb18927f6035
Breno Dias Santos <160603341+brenodiassto@users.noreply.github.com>
2024-03-07 14:35:11 +0000

    atualização 2

---
+++ temas.js
@@ -0,0 +1,13 @@
+document.querySelector('#btTrocaTema').addEventListener('click', function ()  {
+    //pegar elemento com o tema
+    //se o tema for light, entao mude o atributo para dark
+    //se nao for, entao mude o atributo para light
+
+    if (document.documentElement.getAttribute('data-tema') === 'light') {
+        document.documentElement.setAttribute('data-tema', 'dark');
+      }  else {
+            document.documentElement.setAttribute('data-tema', 'light');
+        } 
+        
+    
+});
